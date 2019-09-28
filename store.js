ea0db89cc0c25354b3ea238fcaa393880b8f965c
Peggy Sturman <glamazon5@gmail.com>
2019-09-28 11:58:41 -0700

    Quiz files, Wireframe file and JS added

---
+++ store.js
@@ -0,0 +1,157 @@
+const STORE = {
+  questions:[ 
+  //Question 1
+    {
+    question: "Johnny Cash married his second wife in 1968. She was part of one of the most well known musical groups in country music history. What was her name?",
+  answers: [
+    "Dolly Parton",
+    "June Carter",
+    "May Carter",
+    "Olivia Newton John"
+  ],
+  correctAnswer:
+  "June Carter"
+},
+
+    //Question 2
+    {
+      question: "Singer George Jones went by this animal nickname?",
+      answers: [
+        "Possum",
+        "Panda Bear",
+        "Wombat",
+        "Honey Badger"
+      ],
+      correctAnswer:
+        "Possum"
+    },
+
+    //Question 3
+    {
+      question: "Which of these singers was NOT a member of The Highwaymen?",
+      answers: [
+        "Willie Nelson",
+        "Kris Kristofferson",
+        "Merle Haggard",
+        "Waylon Jennings"
+      ],
+      correctAnswer:
+        "Merle Haggard"
+    },
+
+    //Question 4
+    {
+      question: "This singer was supposed to be on the same plane that crashed and killed Buddy Holly, but he gave his seat up to the Big Bopper--who also died in the crash.",
+      answers: [
+        "George Jones",
+        "Waylon Jennings",
+        "Porter Wagoner",
+        "Johnny Cash"
+      ],
+      correctAnswer:
+        "Waylon Jennings"
+    },
+
+    //Question 5
+    {
+      question: "What is Johnny Paycheck's real name?",
+      answers: [
+        "Henry Deutschendorf Jr.",
+        "Hiram Williams",
+        "Donald Lytle",
+        "Ira Loudermilk"
+      ],
+      correctAnswer:
+        "Donald Lytle"
+    },
+
+    //Question 6
+    {
+      question: "Who wrote Johnny Paycheck's 1977 number one hit song“Take This Job and Shove It?",
+      answers: [
+        "Tom T. Hall",
+        "David Allan Coe",
+        "Guy Clark",
+        "Swamp Dogg"
+      ],
+      correctAnswer:
+        "David Allan Coe"
+    },
+
+    //Question 7
+    {
+      question: "At which prison did Johnny Cash NOT record a live album?",
+      answers: [
+        "Tennessee State Prison",
+        "Folsom State Prison,
+        "San Quentin State Prison",
+        "Leavenworth Federal Penitentiary"
+      ],
+      correctAnswer:
+        "Tennessee State Prison"
+    },
+
+    //Question 8
+    {
+      question: "After his wife hid all of his car keys, what mode of transportation did George Jones famously use to get to the liquor store 8 miles away?",
+      answers: [
+        "A horse",
+        "A jet ski",
+        "A riding lawnmower",
+        "Roller skates"
+      ],
+      correctAnswer:
+        "A riding lawnmower"
+    },
+
+    //Question 9
+    {
+      question: "Singer and songwriter Gram Parsons overdosed on morphine in 1973 during a trip to visit which national park?",
+      answers: [
+        "Big Bear",
+        "Grand Canyon",
+        "Joshua Tree",
+        "White Sands"
+      ],
+      correctAnswer:
+        "Joshua Tree"
+    },
+
+    //Question 10
+    {
+      question: "In 1975, Hank Williams Jr. almost died accidentally while doing what activity?",
+      answers: [
+        "Motorcycle stunts",
+        "Rock climbing",
+        "Fleeing the police to avoid a DUI",
+        "Shaving"
+      ],
+      correctAnswer:
+        "Rock climbing"
+    },
+
+    //Question 11
+    {
+      question: "In 1981 Johnny Cash was hospitalized after being attacked by an animal from his own personal zoo. What kind of animal was it?",
+      answers: [
+        "Alligator",
+        "Ostrich",
+        "Chimpanzee",
+        "Wolverine"
+      ],
+      correctAnswer:
+        "Ostrich"
+    },
+
+    //Question 12
+    {
+      question: "Name the first track off of Willie Nelson's 1975 album“Red Headed Stranger.,
+      answers: [
+        "Red Headed Stranger",
+        "Blue Eyes Cryin' in the Rain",
+        "Time of the Preacher",
+        "Whiskey River"
+      ],
+      correctAnswer:
+        "Time of the Preacher"
+    },
